28be6cdafbdf11d36019599ccc06fcd67b06fd6f
carlettisantiago <carlettisantiago@gmail.com>
2022-08-16 17:15:56 -0300

    primera subida

---
+++ Unidad 1/Ejercicio 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+    //ENUNCIADO EJERCICIO 1
+    //Crear un proyecto de consola .Net Framework y ejecutar el "Hola Mundo".
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("¡Hola Mundo!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Unidad 1/Ejercicio 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio2
+{
+    //ENUNCIADO EJERCICIO 2
+    //Crear un proyecto de WinForm con .Net Framework y ejecutar nuestra primera ventana gráfica.
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
