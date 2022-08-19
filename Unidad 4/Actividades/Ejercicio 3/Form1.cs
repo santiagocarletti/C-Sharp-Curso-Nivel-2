b5154f8124b4a936b098d5fd155448bafe7f17e8
Santiago Carletti <111025122+santiagocarletti@users.noreply.github.com>
2022-08-18 23:12:16 -0300

    Update Form1.cs

--- Unidad 4/Actividades/Ejercicio 3/Form1.cs
+++ Unidad 4/Actividades/Ejercicio 3/Form1.cs
@@ -10,44 +10,6 @@ using System.Windows.Forms;
 
 namespace Practica3LabelYLinkLabel
 {
-    //TEMATICA EJERCICIO 3: CONTROL LABEL Y LINKLABEL
-    //
-    //ENUNCIADO:
-    //1. Agregar un Label al Formulario.
-    //2. Analizamos el archivo Form1.Designer.cs.
-    //3. Cambiamos el nombre del Label, con la propiedad Name. 
-    //4. Cambiamos el Texto del Label con la propiedad Text.
-    //5. Desplegamos el Cuadro de Dialogo de la propiedad Font y cambiamos Tipo Letra,
-    //Tamaño y estilo.
-    //6. Cambiamos el color del Texto, desplegando la paleta de colores personalizados de
-    //la propiedad ForeColor. 
-    //7. Aplicamos un borde 3D con la propiedad BorderStyle. 
-    //8. Manejar el evento MouseMove del Control Label.En la ventana de eventos elijo
-    //MouseMove.
-    //private void lblEtiqueta_MouseMove(object sender, MouseEventArgs e)
-    //    {
-    //    } 
-    //9. Cambiamos el color de la propiedad BackColor.
-    //private void lblEtiqueta_MouseMove(object sender, MouseEventArgs e)
-    //    {
-    //        lblEtiqueta.BackColor = Color.Cyan;
-    //    } 
-    //10.Al dispararse el evento MouseLeave, se restablezca el color de fondo de la etiqueta.
-    //private void lblEtiqueta_MouseLeave(object sender, EventArgs e)
-    //    {
-    //        lblEtiqueta.BackColor = System.Drawing.SystemColors.Control;
-    //    } 
-    //11.Cambiar el estilo del cursor al dispararse los eventos MouseMove y MouseLeave.
-    //private void lblEtiqueta_MouseMove(object sender, MouseEventArgs e)
-    //    {
-    //        lblEtiqueta.BackColor = Color.Cyan;
-    //        lblEtiqueta.Cursor = Cursors.Hand;
-    //    }
-    //    private void lblEtiqueta_MouseLeave(object sender, EventArgs e)
-    //    {
-    //        lblEtiqueta.BackColor = System.Drawing.SystemColors.Control;
-    //        lblEtiqueta.Cursor = Cursors.Arrow;
-    //    }
     public partial class Form1 : Form
     {
         public Form1()
